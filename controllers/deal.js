924e83b3ad12f6e951f94b890e75ea9ee69b9563
Thibeault Chenu <contact@thibeaultchenu.fr>
2019-04-07 17:46:32 +0200

    Use POST parameters

--- controllers/deal.js
+++ controllers/deal.js
@@ -3,7 +3,7 @@ const dealspeaker = require('./../src/dealspeaker')
 const DEFAULT_LIMIT = 1
 
 exports.index = (req, res, next) => {
-    let customLimit = typeof req.query.number === 'undefined' ? DEFAULT_LIMIT : req.query.number
+    let customLimit = typeof req.body.number === 'undefined' ? DEFAULT_LIMIT : req.body.number
     dealabs.getDeals(customLimit, (err, data) => {
       let fulfillmentText = 'An error is occured'
       dealspeaker.convert(data, (fulfillmentText) => {
