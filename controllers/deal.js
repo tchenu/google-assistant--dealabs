8720bb1e8d115894ced018d1899eacba36bc9eb9
Thibeault Chenu <contact@thibeaultchenu.fr>
2019-04-07 16:45:20 +0200

    debug for DialogFlow

--- controllers/deal.js
+++ controllers/deal.js
@@ -7,7 +7,8 @@ exports.index = (req, res, next) => {
       if (err) {
         res.json({status: 500, message: err.message, data: null})
       } else {
-        res.json({ status: 300, message: 'Success', data: data })
+        res.json({ 'speech': data[0]['title'], 'displayText': data[0]['title']})
+        // res.json({ status: 300, message: 'Success', data: data })
       }
     })
 }
